f3dca1f94858b5afbdb79159f537bfd21eeacf71
Gladys Ange Isingizwe <gladyisingizwe@gmail.com>
2023-01-05 20:36:13 -0500

    fixed the answer displaying issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -236,7 +236,7 @@ function showQuestion(question){
     console.log("questions start here")
     questionElement.innerText = shuffledQuestions[currentQuestionIndex];
     // loop through answers and populate them and create a button for each
-    question.answers.forEach(answer => {
+    questions[currentQuestionIndex].answers.forEach(answer => {
     // answersValues.forEach(answer => {
         var button = document.createElement("button")
         button.innerText = answer.text
